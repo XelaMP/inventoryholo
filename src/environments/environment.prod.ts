e0606d2ff7d1e867fd50e052eb3a00f2f0d891f9
XelaMP <alexanderpisconti@gmail.com>
2021-02-06 14:25:54 -0500

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:8000/api',
+  api: 'https://api-inventory.holosalud.org/api',
   SECRET_API_FILES: 'miclavedetokens',
   api_files: 'https://api-files.holosalud.org',
 
